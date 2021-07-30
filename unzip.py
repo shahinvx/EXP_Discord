089892461e13b2854a852681acd9abc902e89c5f
Md. Mahmudul Hasan Shahin <shahinvx@gmail.com>
2021-07-30 16:00:16 +0600

    Add files via upload

---
+++ unzip.py
@@ -0,0 +1,9 @@
+import patoolib
+patoolib.extract_archive("./discord_app.rar", outdir="./")
+
+'''
+import zipfile
+with zipfile.ZipFile('./discord_app.zip', 'r') as zip_ref:
+    zip_ref.extractall('./')
+	
+'''
